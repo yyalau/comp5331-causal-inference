3cbd95e2bef9f0575343a35f18b8b281d5352b0c
DarkLight1337 <cyrus.tl.leung@gmail.com>
2023-10-11 22:37:34 +0800

    Set up default logger for CLI

--- run_erm.py
+++ run_erm.py
@@ -1,17 +1,30 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from lightning.pytorch.cli import LightningCLI
 
 # from src.dataops.datamodules import ERMDataModule
 from src.modelops.tasks import ERMTask
 
 def cli():
+    experiment_name = 'erm'
+    checkpoint_dir = Path(__file__).parent / 'data' / experiment_name
+
     LightningCLI(
         model_class=ERMTask,
         # datamodule_class=ERMDataModule,
+        trainer_defaults={
+            'logger': {
+                'class_path': 'TensorBoardLogger',
+                'init_args': {
+                    'save_dir': checkpoint_dir,
+                    'name': experiment_name,
+                },
+            },
+        },
         auto_configure_optimizers=False,
     )
-
 
 if __name__ == '__main__':
     cli()
--- run_fast.py
+++ run_fast.py
@@ -1,17 +1,30 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from lightning.pytorch.cli import LightningCLI
 
 # from src.dataops.datamodules import FASTDataModule
 from src.modelops.tasks import FASTTask
 
 def cli():
+    experiment_name = 'fast'
+    checkpoint_dir = Path(__file__).parent / 'data' / experiment_name
+
     LightningCLI(
         model_class=FASTTask,
         # datamodule_class=FASTDataModule,
+        trainer_defaults={
+            'logger': {
+                'class_path': 'TensorBoardLogger',
+                'init_args': {
+                    'save_dir': checkpoint_dir,
+                    'name': experiment_name,
+                },
+            },
+        },
         auto_configure_optimizers=False,
     )
-
 
 if __name__ == '__main__':
     cli()
