d37ceaddf8e721fe032adb8dde23c4748750dfc9
Tony Tong <ztangaj@connect.ust.hk>
2023-10-17 04:33:15 +0800

    change of fast

--- src/models/fa/fast.py
+++ src/models/fa/fast.py
@@ -4,6 +4,8 @@ from typing import Generic, TypeVar
 
 from torch import Tensor
 import torch.nn as nn
+from torchvision.models import resnet18
+from torchvision.transforms import Normalize
 
 from modelops.dependencies import ClassificationModel, FAST_X, Classification_Y
 
@@ -17,7 +19,10 @@ class FAST(nn.Module, Generic[NST, Classification_Output]):
     def __init__(self,
                 style_transfer: NST,
                 classifer: ClassificationModel[FAST_X],
+                model = resnet18,
                 device: str = 'cpu', # "cpu" for cpu, "cuda" for gpu
+                pixel_mean: float[3] = [0.5, 0.5, 0.5], # mean for normolization
+                pixel_std: float[3] = [0.5, 0.5, 0.5], # std for normolization
                 gamma: float = 2.0, # Controls importance of StyleLoss vs ContentLoss, Loss = gamma*StyleLoss + ContentLoss
                 training: bool =True, # Wether or not network is training
                 scr_temperature: float = 0.1,
@@ -27,15 +32,25 @@ class FAST(nn.Module, Generic[NST, Classification_Output]):
 
         self.style_transfer = style_transfer(gamma, training, scr_temperature).to(device)
         self.classifier = classifer
+        self.model = model
+        self.normalization = Normalize(mean = pixel_mean, std = pixel_std)
 
 
     def forward(self, input: FAST_X) -> Classification_Output:
         content = input.get('content')
         styles = input.get('styles')
 
-        transferred_contents: Tensor = self.style_transfer(content, styles)
+        # transferred_contents: Tensor = self.style_transfer(content, styles)
+        fx_tildes = []
+        for x_prime in styles:
+            x_tilde = self.style_transfer(content, x_prime)
+            fx_tilde = self.model(self.normalization(x_tilde))
+            fx_tildes.append(fx_tilde)
 
-        classifier_input: FAST_X = {'content': transferred_contents, 'styles': styles}
+        fx = self.model(self.normalization(content))
+        weighted_output = fx*self.beta+(1-self.beta)*sum(fx_tildes)/len(fx_tildes)
+
+        classifier_input: FAST_X = {'content': weighted_output, 'styles': styles}
         predictions: Classification_Output = self.classifier(classifier_input)
 
         return predictions
