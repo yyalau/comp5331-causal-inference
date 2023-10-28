b36a18dd8105c00c0269e4f99a636d57cca83f6b
Tony Tong <ztangaj@connect.ust.hk>
2023-10-28 19:54:19 +0800

    run_nst

--- run_nst.sh
+++ run_nst.sh
@@ -1,9 +1,14 @@
-DIR=$(realpath ".")
+# DIR=$(realpath ".")
 # DIR=$(dirname "${HERE}")
 
-python "$DIR/run_nst.py" fit \
-    --data "$DIR/config/data/digits.yaml" \
-    --model "$DIR/config/model/nst/ada_in.yaml" \
+# SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+echo "Script directory: $SCRIPT_DIR"
+
+
+python "$SCRIPT_DIR/run_nst.py" fit \
+    --data "$SCRIPT_DIR/config/data/office_home.yaml" \
+    --model "$SCRIPT_DIR/config/model/nst/ada_in.yaml" \
     --trainer.devices 1 \
     --trainer.accelerator gpu 
 # python /home/dycpu3_data1/yyalau/comp5331/run_nst.py fit \
