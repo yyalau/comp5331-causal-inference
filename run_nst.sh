cea895a1a2223ddb93c59e9a90bfc5af360da9d8
Tony Tong <ztangaj@connect.ust.hk>
2023-11-01 23:02:36 +0800

    enable the models

--- run_nst.sh
+++ run_nst.sh
@@ -8,9 +8,10 @@ cd "$SCRIPT_DIR"
 
 
 python "$SCRIPT_DIR/run_nst.py" fit \
-    --data "$SCRIPT_DIR/config/data/office_home.yaml" \
+    --data "$SCRIPT_DIR/config/data/digits.yaml" \
     --model "$SCRIPT_DIR/config/model/nst/ada_in.yaml" \
     --trainer.devices 1 \
-    --trainer.accelerator gpu 
+    --trainer.accelerator gpu \
+    --data.batch_size 8
 # python /home/dycpu3_data1/yyalau/comp5331/run_nst.py fit \
 #     --help
