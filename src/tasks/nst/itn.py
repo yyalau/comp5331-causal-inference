5bc996d15dafd2ba1dffb99dff382ea2ca21dcb8
Tony Tong <ztangaj@connect.ust.hk>
2023-11-02 23:35:51 +0800

    itn

--- src/tasks/nst/itn.py
+++ src/tasks/nst/itn.py
@@ -10,7 +10,7 @@ from torch.optim.lr_scheduler import LRScheduler
 from torch.utils.tensorboard.writer import SummaryWriter
 from lightning.pytorch.loggers import TensorBoardLogger
 
-from ...models.nst import StyleTransfer_X, StyleTransfer_Y, AdaINModel
+from ...models.nst import StyleTransfer_X, StyleTransfer_Y, ItnModel
 
 from ..base import EvalOutput, BaseTask
 
@@ -29,7 +29,8 @@ class ItnTask(BaseTask[StyleTransfer_X, ItnEvalOutput, StyleTransfer_X, StyleTra
         *,
         optimizer: Callable[[Iterable[torch.nn.Parameter]], Optimizer],
         scheduler: Callable[[Optimizer], LRScheduler],
-        gamma: float = 2.0,
+        w_style: float = 1.0,
+        w_content: float = 20.0,
         img_log_freq: int = 64,
     ) -> None:
         super().__init__(
@@ -39,96 +40,180 @@ class ItnTask(BaseTask[StyleTransfer_X, ItnEvalOutput, StyleTransfer_X, StyleTra
 
         self.network = network
 
-        self.gamma = gamma
-
         self.loss = self._combined_loss
         self.metrics = {
             'content_loss': self._content_loss,
             'style_loss': self._style_loss,
+            'category_loss': self._category_loss,
+            'cam_loss': self._cam_loss,
         }
 
         self.img_log_freq = img_log_freq
 
-        # hyperparameters: TODO: move to config file
-        # self.content_weight = 1 # default value
-        # self.style_weight = 20 # default value
-        # self.learning_rate = 1e-3 # default value
-
-
         # cam
-        self.weight_softmax = torch.squeeze(list(self.squeeze_net.parameters())[-2])
+        self.w_softmax = torch.squeeze(list(self.network.squeeze_net.parameters())[-2])
+        
 
-        # style image
-        # TODO: some preprocessing for self.style / better code for this part
-        style = ut.tensor_load_rgbimage(style_image, size=style_size)
-        style = ut.preprocess_batch(style.repeat(self.batch_size, 1, 1, 1))
-        self.gram_style = [ut.gram_matrix(y) for y in self.vgg16(ut.subtract_imagenet_mean_batch(style))]
+    def _content_loss(self, feat_x: StyleTransfer_Y, feat_y: StyleTransfer_Y) -> torch.Tensor:
+        # self.network.vgg16(y_hat)
+        return F.mse_loss(feat_x[2], feat_y[2])
+    
+
+    def _style_loss(self, feat_xc, feat_xs: StyleTransfer_Y) -> torch.Tensor:
+        
+        def gram_matrix(y):
+            (b, ch, h, w) = y.size()
+            features = y.view(b, ch, w * h)
+            features_t = features.transpose(1, 2)
+            gram = features.bmm(features_t) / (ch * h * w)
+            return gram 
+    
+        style_loss = 0            
+        for x,y in zip(feat_xc, feat_xs):
+            style_loss += F.mse_loss(*map( gram_matrix, (x, y)))
+            
+        return style_loss
+    
+    def _category_loss(self, y_hat: StyleTransfer_Y, y: StyleTransfer_Y) -> torch.Tensor:
+        return F.cross_entropy(y_hat, y)
+    
+    def _cam_loss(self, label_id_x: StyleTransfer_Y, label_id_y: StyleTransfer_Y) -> torch.Tensor:
+        
+        def get_cam(feature_conv, class_idx):
+            b, nc, h, w = feature_conv.shape #8, 512, 1, 1
+            cam = torch.einsum('...ij,...jk->ik', 
+                               self.w_softmax[class_idx], 
+                               feature_conv.view(b, nc, -1))
+            return cam
+        
+        x_cam = get_cam(self.network.features_blobs[0], label_id_x)
+        y_cam = get_cam(self.network.features_blobs[1], label_id_y)
+        
+        return F.mse_loss(x_cam, y_cam)
+    
+    def _combined_loss(self, features_xc, features_yc, features_xs, label_id_xc, label_id_yc, logit_yc) -> torch.Tensor:
+        content_loss = self._content_loss(features_xc, features_yc) 
+        style_loss = self._style_loss(features_yc, features_xs)
+        cam_loss = self._cam_loss(label_id_xc, label_id_yc)
+        category_loss = self._category_loss(logit_yc, label_id_xc)
+
+        return 10000*category_loss + 80* cam_loss + self.w_style * style_loss + self.w_content * content_loss
+
+    def _eval_step(self, batch, batch_idx) -> ItnEvalOutput:
+        
+        
+        '''
+        features: pass through vgg
+        logit: pass through squeeze net
+        y: pass through transfer model
+        x: original batch data
+        c: content
+        s: style
+        '''
+
+        ## TODO: refactor this part
+        if self.w_softmax.device != batch['content'].device:
+            self.w_softmax = self.w_softmax.to(batch['content'].device)
+        
+        
+        def get_rank(logits):
+            h = F.softmax(logits, dim=1)
+            _, idx = h.sort(dim = 1, descending = True)
+            return idx[:,0]
+        
+        
+        xc, xs = batch['content'], batch['style']
+        yc = self.network.transfer_model(xc) # 8, 3, 32,32
+        
+        logit_xc, logit_yc = map(self.network.squeeze_net, (xc, yc)) # 8, 1000
+        label_id_xc, label_id_yc = map(get_rank, (logit_xc, logit_yc))
+        
+
+        features_xc, features_yc = map(self.network.vgg16.get_states, (xc, yc)) # 8, 1000
+        features_xs = self.network.vgg16.get_states(xs)
+
+        
+        # 8, 256, 32, 32-> 8, 256, 16, 16 -> 8, 256, 8, 8 -> 8, 256, 4, 4
+        loss = self.loss(features_xc, features_yc, features_xs, label_id_xc, label_id_yc, logit_yc)
+        metrics = {name: metric(features_xc, features_yc, features_xs, label_id_xc, label_id_yc, logit_yc) 
+                   for name, metric in self.metrics.items()}
+        
+        import ipdb; ipdb.set_trace()
+        
+        return ItnEvalOutput(loss=loss, metrics=metrics, x=batch, lazy_y_hat=lambda: self.network(batch))
+    
+
+    def forward(self, x: StyleTransfer_X) -> StyleTransfer_Y:
+        return self.network(x)
 
 
+    def _process_images(self, eval_output: AdaINEvalOutput, *, prefix: str, batch_idx: int) -> None:
+        if batch_idx % self.img_log_freq:
+            return
 
-    def hook_feature(self, module, input, output):
-        self.features_blobs.append(output.data.cpu().numpy())
+        if isinstance(self.logger, TensorBoardLogger):
+            self._log_images(self.logger.experiment, eval_output, prefix=prefix)
+        else:
+            raise TypeError('Incorrect type of logger')
 
-    def _returnCAM(self, feature_conv, weight_softmax, class_idx):
-        # generate the class activation maps upsample to 256x256
-        nc, h, w = feature_conv.shape
-        cam = torch.matmul(weight_softmax[class_idx],feature_conv.view(nc, h*w))
-        return cam
+    def _log_images(self, writer: SummaryWriter, eval_output: AdaINEvalOutput, *, prefix: str) -> None:
+        eval_output_x_content = eval_output.x['content'].detach().cpu().float()
+        eval_output_x_style = eval_output.x['style'].detach().cpu().float()
+        eval_output_y_hat = eval_output.lazy_y_hat().detach().cpu().float()
+        batch_size = eval_output_y_hat.shape[0]
 
-    def training_step(self, batch, batch_idx):
+        nrows = batch_size
+        ncols = 3
+        fig, axes = plt.subplots(
+            nrows=nrows, ncols=ncols,
+            sharex='col', sharey='col',
+            squeeze=False,
+            figsize=((ncols * 4), (nrows * 4)),
+        )
 
-        ### cam loss
-        y = self.forward(batch)
-        y_cam = ut.subtract_mean_std_batch(ut.preprocess_batch(y))
+        grid_idx = 1
+        for row in range(nrows):
+            example_input_content = torch.einsum('chw->hwc', eval_output_x_content[row])
+            example_input_style = torch.einsum('chw->hwc', eval_output_x_style[row])
+            example_output_applied = torch.einsum('chw->hwc', eval_output_y_hat[row])
 
-        x = batch.clone()
-        x_cam = ut.subtract_mean_std_batch(ut.preprocess_batch(x))
+            for col in range(ncols):
+                ax: Axes = axes[row, col]
 
-        logit_x = self.squeeze_net(x_cam); logit_y = self.squeeze_net(y_cam)
+                if row == 0:
+                    if col == 0:
+                        ax.set_title('Content Image')
+                    elif col == 1:
+                        ax.set_title('Style Image')
+                    elif col == 2:
+                        ax.set_title('Applied Image')
 
+                if col == 0:
+                    ax.imshow(example_input_content)
+                elif col == 1:
+                    ax.imshow(example_input_style)
+                elif col == 2:
+                    ax.imshow(example_output_applied)
 
-        cam_loss = 0
-        label = []
+                grid_idx += 1
 
-        ## cam loss and category loss
-        for i in range(len(x_cam)):
-            h_x  = F.softmax(logit_x[i])#.data.squeeze()
-            probs_x, idx_x = h_x.sort(0, True)
+        fig.tight_layout()
 
-            h_y  = F.softmax(logit_y[i])#.data.squeeze()
-            probs_y, idx_y = h_y.sort(0, True)
+        writer.add_figure(f'images/{prefix}batch', fig, global_step=self.global_step)
 
-            x_cam = self._returnCAM(self.features_blobs[0][i], self.weight_softmax, idx_x[0])
-            y_cam = self._returnCAM(self.features_blobs[0][i], self.weight_softmax, idx_y[0])
+    def training_step(self, batch: StyleTransfer_X, batch_idx: int) -> dict[str, torch.Tensor]:
+        eval_output = self._eval_step(batch, batch_idx)
+        self._process_images(eval_output, batch_idx=batch_idx, prefix='train_')
+        return self._process_eval_loss_metrics(eval_output, prefix='')
 
-            cam_loss += F.mse_loss(x_cam, y_cam)
-            label.append(idx_x[0])
+    def validation_step(self, batch: StyleTransfer_X, batch_idx: int) -> dict[str, torch.Tensor]:
+        eval_output = self._eval_step(batch, batch_idx)
+        self._process_images(eval_output, batch_idx=batch_idx, prefix='val_')
+        return self._process_eval_loss_metrics(eval_output, prefix='val_')
 
-        cam_loss *= 80
-        category_loss = 10000 * F.cross_entropy(logit_y, torch.LongTensor(label).to(self.device))
+    def test_step(self, batch: StyleTransfer_X, batch_idx: int) -> dict[str, torch.Tensor]:
+        eval_output = self._eval_step(batch, batch_idx)
+        self._process_images(eval_output, batch_idx=batch_idx, prefix='test_')
+        return self._process_eval_loss_metrics(eval_output, prefix='test_')
 
-
-
-        ### content loss
-        x = ut.subtract_imagenet_mean_batch(x)
-        y = ut.subtract_imagenet_mean_batch(y)
-        features_x = self.vgg16(x)
-        features_y = self.vgg16(y)
-
-        content_loss = self.content_weight * F.mse_loss(features_x[2].data, features_y[2])
-
-
-        ### style loss
-        style_loss = 0
-        for m in range(len(features_x)):
-            gram_y = ut.gram_matrix(features_y[m])
-            style_loss += self.style_weight* F.mse_loss(gram_y, self.gram_style[m].data[:self.batch_size*batch_idx, :, :])
-
-
-        ### total loss
-        total_loss = cam_loss + category_loss + content_loss + style_loss
-        return total_loss
-
-    def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
-        return optimizer
+    
