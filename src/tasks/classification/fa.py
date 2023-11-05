9fbc16c2824ec767eb53bb161c0f9f243f376646
DarkLight1337 <cyrus.tl.leung@gmail.com>
2023-11-05 17:08:02 +0800

    Update logging to also show style transferred images

--- src/tasks/classification/fa.py
+++ src/tasks/classification/fa.py
@@ -10,7 +10,7 @@ from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LRScheduler
 from torch.utils.tensorboard.writer import SummaryWriter
 
-from ...models.classification.fa import FA_X, FAModel
+from ...models.classification.fa import FA_X, FAModel, FAST, FAFT, FAGT
 
 from .base import ClassificationTask, ClassificationEvalOutput
 
@@ -47,8 +47,16 @@ class FATask(ClassificationTask[FA_X]):
         num_classes = eval_output_y_hat.shape[1]
         num_styles = len(eval_output_x_styles)
 
+        x_tildes = self.classifier.get_x_tildes(eval_output.x) if isinstance(self.classifier, (FAST, FAGT)) else []
+        x_nsts = [x_tilde.detach().cpu().float()[:num_examples] for x_tilde in x_tildes]
+        num_nst_applied = len(x_nsts)
+
+        x_hats = self.classifier.get_x_hats(eval_output.x) if isinstance(self.classifier, (FAFT, FAGT)) else []
+        x_fsts = [x_hat.detach().cpu().float()[:num_examples] for x_hat in x_hats]
+        num_fst_applied = len(x_fsts)
+
         nrows = num_examples
-        ncols = 2 + num_styles
+        ncols = 1 + num_styles + num_nst_applied + num_fst_applied + 1
         fig, axes = plt.subplots(
             nrows=nrows, ncols=ncols,
             sharex='col', sharey='col',
@@ -61,27 +69,41 @@ class FATask(ClassificationTask[FA_X]):
         for row in range(nrows):
             example_input_content = torch.einsum('chw->hwc', eval_output_x_content[row])
             example_input_styles = [torch.einsum('chw->hwc', style[row]) for style in eval_output_x_styles]
+
+            example_x_nsts = [torch.einsum('chw->hwc', x_nst[row]) for x_nst in x_nsts]
+            example_x_fsts = [torch.einsum('chw->hwc', x_fst[row]) for x_fst in x_fsts]
+
             example_gt_class_idx = eval_output_y[row].argmax().item()
             example_class_prob = torch.softmax(eval_output_y_hat[row].unsqueeze(1), dim=0)
             example_pred_class = example_class_prob.argmax().item()
 
             for col in range(ncols):
                 ax: Axes = axes[row, col]
-                style_idx = col - 1
 
                 if row == 0:
-                    if style_idx < 0:
+                    if col == 0:
                         ax.set_title('Content Image')
-                    elif style_idx < num_styles:
+                    elif 1 <= col < 1 + num_styles:
                         ax.set_title('Style Image')
-                    elif style_idx == num_styles:
+                    elif 1 + num_styles <= col < 1 + num_styles + num_nst_applied:
+                        ax.set_title('Applied Image (NST)')
+                    elif 1 + num_styles + num_nst_applied <= col < 1 + num_styles + num_nst_applied + num_fst_applied:
+                        ax.set_title('Applied Image (FST)')
+                    elif col == num_styles + num_nst_applied <= col < 1 + num_styles + num_nst_applied + num_fst_applied:
                         ax.set_title('Class Probabilities')
 
-                if style_idx < 0:
+                if col == 0:
                     ax.imshow(example_input_content)
-                elif style_idx < num_styles:
+                elif 1 <= col < 1 + num_styles:
+                    style_idx = col - 1
                     ax.imshow(example_input_styles[style_idx])
-                elif style_idx == num_styles:
+                elif 1 + num_styles <= col < 1 + num_styles + num_nst_applied:
+                    style_idx = col - 1 - num_styles
+                    ax.imshow(example_x_nsts[style_idx])
+                elif 1 + num_styles + num_nst_applied <= col < 1 + num_styles + num_nst_applied + num_fst_applied:
+                    style_idx = col - 1 - num_styles - num_nst_applied
+                    ax.imshow(example_x_fsts[style_idx])
+                elif col == num_styles + num_nst_applied <= col < 1 + num_styles + num_nst_applied + num_fst_applied:
                     ax.imshow(example_class_prob, aspect='auto')
 
                     for c_idx in list(range(num_classes)):
